22d5d15683b6c0e4a99782e1133e12dbc86efe64
Manideep4942 <103894020+Manideep4942@users.noreply.github.com>
2024-01-20 11:27:54 +0530

    added some symptoms

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
  // Define the data object for the expert system
  var data = {
-    symptoms: ["fever", "cough", "sore-throat", "runny-nose", "headache", "loss-of-taste-or-smell"],
+    symptoms: ["fever", "cough", "sore-throat", "runny-nose", "headache", "loss-of-taste-or-smell","cold","kidney"],
     rules: [
       {
-        if: ["fever", "cough", "sore-throat", "loss-of-taste-or-smell"],
-        then: "You may have COVID-19. Please consult a doctor and get tested."
+        if: ["kidney"],
+        then: " To maintain kidney health . keep active and fit and also manage you blood sugar dont smoke "
       },
       {
-        if: ["fever", "cough", "runny-nose"],
-        then: "You may have a common cold. Drink plenty of fluids and rest."
+        if: ["loss-of-taste-or-smell"],
+        then: " you may have COVID 19 so please be sure to check ones covid test and consult the doctor"
       },
       {
-        if: ["fever", "headache"],
-        then: "You may have a fever. Take some paracetamol and monitor your temperature."
+        if: ["cold"&&"cough"],
+        then: " monitor the temperature. and use the cool mist humidifiers and get plenty of rest"
+      },
+      {
+        if: ["fever"&&"headache"],
+        then: " Take some paracetamol and monitor your temperature. drink some mint water to reduce the headache"
+      },
+      {
+        if: ["cough"],
+        then: "You may have a some fever and  cough. Take some paracetamol beacuse it may increase thr fever by the cough  drink the Hot Hot fluieds." 
+      },
+      {
+        if: ["cough"&&"fever"],
+        then: "You may have a some fever and  cough. use Paracetamol for pain and fever. ,Combination 'cough and cold' medicines Paracetamol is found in many cold and flu products, eg, Codral®, Coldrex® and Lemsip®.." 
       },
       {
         if: ["sore-throat"],
-        then: "You may have a sore throat. Gargle with salt water and avoid spicy foods."
-      }
+        then: "You may have a sore throat. Gargle with salt water and avoid spicy foods to reduce sore-throat."
+      },
+      {
+        if: ["runny-nose"],
+        then: "You may have a runny-nose. drink plenty of fluids,Hot teas ,Facial steam."
+      },
+      {
+        if: ["headache"],
+        then: "You may have a headche. to reduce the headache naturally drink-water, Take some high magnesium foods get sufficent sleep. Try essintial oils"
+      },
     ]
   };
   // Define the function to diagnose the disease based on the user input and the rules
